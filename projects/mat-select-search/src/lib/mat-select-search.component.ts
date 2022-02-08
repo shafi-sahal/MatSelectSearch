5c91c0af1f4b1d6db780a15c25a8945188f3ac9e
ShafiSahal99 <shafisahal99@gmail.com>
2022-02-08 08:43:45 +0530

    fix: persist selected options on new filtered lists in multi select

--- projects/mat-select-search/src/lib/mat-select-search.component.ts
+++ projects/mat-select-search/src/lib/mat-select-search.component.ts
@@ -3,12 +3,16 @@ import { MatOption } from '@angular/material/core';
 import { MatSelect } from '@angular/material/select';
 import { Subscription } from 'rxjs';
 import { Searcher } from './searcher.service';
+import { filter } from 'rxjs/operators';
+
+const NON_ITEM_OPTIONS_COUNT = 2;
+const INDEX_SELECT_ALL = 1;
 
 @Component({
   selector: 'lib-mat-select-search',
   templateUrl: './mat-select-search.component.html',
   styleUrls: [ './mat-select-search.scss' ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  //changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [Searcher]
 })
 export class MatSelectSearchComponent implements AfterViewInit, OnDestroy {
@@ -21,6 +25,9 @@ export class MatSelectSearchComponent implements AfterViewInit, OnDestroy {
 
   // Make true if input should be cleared on opening
   @Input() clearSearchInput = false;
+
+   // Make true if mat-select has multiple attribute with true value
+  @Input() isMultiSelect = false;
 
   // Make true if there is a mat-option for selecting all values
   @Input() hasSelectAll = false;
@@ -35,6 +42,8 @@ export class MatSelectSearchComponent implements AfterViewInit, OnDestroy {
   private fullList: Record<string, string>[] = [];
   private hasFilteredBefore = false;
   private subscriptions = new Subscription();
+  private selectedOptions: any[] = [];
+  private nativeSelectAllCheckbox!: HTMLElement;
   private clickListenerSelectAll = () => {};
 
   constructor(
@@ -48,18 +57,35 @@ export class MatSelectSearchComponent implements AfterViewInit, OnDestroy {
     this.configMatOption();
     this.fullList = this.list;
     this.searcher.initSearch(this.list, this.searchProperties);
-    this.subscriptions
-      .add(this.matSelect.openedChange
-      .subscribe(() => {
+    this.subscriptions.add(this.matSelect.openedChange.subscribe(() => {
         const input = this.element.nativeElement;
         input.focus();
         if ((this.filteredList && this.filteredList.length === 0 && this.hasFilteredBefore) || this.clearSearchInput) {
           input.value = '';
           this.filtered.emit(this.fullList);
         }
+    }))
+    .add(this.filtered.subscribe(() => this.isLoading = false))
+    .add(this.matSelect.stateChanges.pipe(filter(() => this.hasSelectAll))
+      .subscribe(() => {
+        const matOptions = this.matSelect.options.toArray();
+        const selectAll = matOptions[INDEX_SELECT_ALL]._getHostElement();
+        if (matOptions.length > NON_ITEM_OPTIONS_COUNT) {
+          this.renderer.setStyle(selectAll, 'display', 'flex');
+        } else {
+          this.renderer.setStyle(selectAll, 'display', 'none')
+        }
+
+        const items = matOptions.slice(NON_ITEM_OPTIONS_COUNT);
+        const isAllItemsSelected = items.every(item => item.selected);
+        if (isAllItemsSelected && items.length > NON_ITEM_OPTIONS_COUNT) {
+          this.selectNativeSelectAllCheckbox();
+        } else {
+          this.deselectNativeSelectAllCheckbox();
+        }
       })
-      .add(this.filtered.subscribe(() => this.isLoading = false))
     );
+
     setTimeout(() => this.filtered.emit(this.fullList));
   }
 
@@ -83,7 +109,6 @@ export class MatSelectSearchComponent implements AfterViewInit, OnDestroy {
   }
 
   stopCharPropagation(event: KeyboardEvent): void {
-
     const key = event.key;
     const isTextControlKey = key === ' ' || key === 'Home' || key === 'End' || (key >= 'a' && key <= 'z');
     if (isTextControlKey) { event.stopPropagation(); }
@@ -99,27 +124,96 @@ export class MatSelectSearchComponent implements AfterViewInit, OnDestroy {
     const checkBox = nativeMatOption.childNodes[0];
     this.renderer.removeChild(nativeMatOption, checkBox);
 
+    if (this.isMultiSelect) this.configMultiSelect();
     if (this.hasSelectAll) this.enableSelectAll();
     if (this.fixOnTop) this.fixSearchBarOnTopWhileScroll();
   }
 
+  /*
+    This method is used to retain the old selected options after selecting an option from the new filtered list.
+    The old selected options are stored in selectedOptions and the new matSelect value is appended with selected options.
+  */
+  private configMultiSelect(): void {
+    this.subscriptions.add(this.matSelect.optionSelectionChanges.subscribe(change => {
+      const isSelectAllOption = this.hasSelectAll && change.source.id === 'mat-option-1';
+      if (!change.isUserInput || isSelectAllOption) return;
+      const itemIndex = this.selectedOptions.indexOf(change.source.value);
+      if (itemIndex > -1) {
+        this.selectedOptions.splice(itemIndex, 1);
+      } else {
+        this.selectedOptions.push(change.source.value);
+      }
+      this.matSelect.value = [...this.selectedOptions];
+
+      if (!this.hasSelectAll) return;
+      const selectedOptionsCount = this.matSelect.options.filter(option => option.selected).length;
+      const isAllOptionsSelected =
+        selectedOptionsCount === this.matSelect.options.length - NON_ITEM_OPTIONS_COUNT;
+      if (isAllOptionsSelected) {
+        this.selectNativeSelectAllCheckbox();
+        return;
+      }
+
+      if (this.nativeSelectAllCheckbox.getAttribute('checked')) {
+        this.deselectNativeSelectAllCheckbox();
+      }
+    }));
+  }
+
+  /*
+    This method helps the user to select all the options in a list. It must also be able to retain the old selected options
+    after clicking Select All in new filtered list. But this has a problem.
+    The checkbox before every mat-option is the default checkbox given by angular on a mat-option.
+    This checkbox can only be checked manually by a method option.select().
+    But this method not only checks the chekbox but also updates the value of the matSelect and hence after clicking on it,
+    the matSelect value loses the old selected options and will only have all the options in the new filtered list.
+
+    To overcome this we remove the default checkbox and create a new checkbox of our own. The newly created checkbox has also
+    a slightly different appearance and hence contrasts with the rest of the checkboxes in the options so which is good as the
+    user will get a feel that this particular option(Select All) is different from the rest of the options
+  */
   private enableSelectAll(): void {
-    const selectAll = this.matSelect.options.toArray()[1];
+    const selectAll = this.matSelect.options.toArray()[INDEX_SELECT_ALL];
     const nativeSelectAll = selectAll._getHostElement();
+    const matPseudoCheckbox = nativeSelectAll.childNodes[0];
+    this.renderer.removeChild(nativeSelectAll, matPseudoCheckbox);
+
+    this.nativeSelectAllCheckbox = this.renderer.createElement('input');
+    this.renderer.setAttribute(this.nativeSelectAllCheckbox, 'type', 'checkbox');
+    this.renderer.addClass(this.nativeSelectAllCheckbox, 'native-checkbox');
+    this.renderer.insertBefore(nativeSelectAll, this.nativeSelectAllCheckbox, nativeSelectAll.childNodes[0]);
 
     this.clickListenerSelectAll = this.renderer.listen(nativeSelectAll, 'click', () => {
-      if (selectAll.selected) this.selectAllOptions(); else this.deselectAllOptions();
+      if (this.nativeSelectAllCheckbox.getAttribute('checked')) {
+        this.deselectNativeSelectAllCheckbox();
+        this.deselectAlloptions();
+      } else {
+        this.selectNativeSelectAllCheckbox();
+        this.selectAllOptions();
+      }
     });
   }
 
   private selectAllOptions(): void {
     const matOptions = this.matSelect.options;
-    for (let i = 2; i < matOptions.length; i++) matOptions.toArray()[i].select();
+    const items = matOptions.toArray().slice(NON_ITEM_OPTIONS_COUNT);
+
+    let nonSelectedItems: Record<string, string>[] = [];
+    items.forEach(item => {
+      if (!item.selected) nonSelectedItems.push(item.value);
+    });
+
+    this.selectedOptions = [...this.selectedOptions, ...nonSelectedItems];
+    this.matSelect.value = [...this.selectedOptions];
   }
 
-  private deselectAllOptions(): void {
+  private deselectAlloptions(): void {
     const matOptions = this.matSelect.options;
-    for (let i = 2; i < matOptions.length; i++) matOptions.toArray()[i].deselect();
+    const items = matOptions.toArray().slice(NON_ITEM_OPTIONS_COUNT);
+    const itemValues = items.map(item => item.value)
+    this.matSelect.value = this.selectedOptions = this.selectedOptions.filter(
+      option => !itemValues.includes(option)
+    );
   }
 
   private fixSearchBarOnTopWhileScroll(): void {
@@ -128,6 +222,14 @@ export class MatSelectSearchComponent implements AfterViewInit, OnDestroy {
     this.renderer.setStyle(searchBar, 'top', '0');
     this.renderer.setStyle(searchBar, 'z-index', '1');
     this.renderer.setStyle(searchBar, 'background-color', 'white');
+  }
+
+  private selectNativeSelectAllCheckbox(): void {
+    this.renderer.setAttribute(this.nativeSelectAllCheckbox, 'checked', 'true');
+  }
+
+  private deselectNativeSelectAllCheckbox(): void {
+    this.renderer.removeAttribute(this.nativeSelectAllCheckbox, 'checked');
   }
 
   ngOnDestroy(): void {
